0ac3cde0b8c8aec00fd6888c3d8900e7ceca61ae
Ariadne Engelbrecht <ariadneengelbrecht@dionysos.local>
2023-05-24 17:01:45 +0200

    Switch to Python and Flask, add database.

---
+++ week_11__final-project/spelling_bee/static/js/index.js
@@ -0,0 +1,92 @@
+const APIurl = 'https://urban-dictionary7.p.rapidapi.com/v0/define?term=';
+
+const controller = new AbortController();
+const signal = controller.signal;
+
+function abortFetching() {
+  console.log('Now aborting');
+  controller.abort();
+}
+
+function setInnerHTMLtextarea(id, APIValue) {
+  const term = document.getElementById('input-word').value;
+  fetchData(term);
+  document.getElementById(id).innerHTML = APIValue;
+  abortFetching();
+}
+
+async function fetchData(term) {
+  const response = await fetch(APIurl + term, {
+    method: 'get',
+    signal: signal,
+  });
+  if (!response.ok) {
+    console.log(response.status, response.statusText);
+  } else {
+    const { list } = await response.json();
+    if (list.length > 0) {
+      const { definition, example, permalink } = list[0];
+      console.log(definition, example);
+      setInnerHTMLtextarea('definition-area', definition);
+      setInnerHTMLtextarea('example-area', example);
+      setInnerHTMLtextarea('source', permalink);
+    }
+  }
+}
+
+const letters = ['b1', 'b2', 'b3', 'b4', 'b5', 'b6', 'b7'];
+
+letters.map((letter) => {
+  const element = document.getElementById(letter);
+  if (element) {
+    element.addEventListener('click', letterValueInput);
+  }
+  console.log(letter);
+});
+
+function letterValueInput() {
+  const input = document.getElementById('input-word');
+  const term = this.innerHTML;
+  input.value += term;
+  return term;
+}
+
+const deleteButton = document.getElementById('delete');
+if (deleteButton) {
+  deleteButton.addEventListener('click', deleteLetter);
+}
+
+function deleteLetter() {
+  const input = document.getElementById('input-word');
+  input.value = input.value.slice(0, -1);
+}
+
+const buttonSubmit = document.getElementById('submit');
+if (buttonSubmit) {
+  buttonSubmit.addEventListener('click', fetchAPI);
+  buttonSubmit.addEventListener('click', appendWord);
+}
+
+function fetchAPI() {
+  const term = document.getElementById('input-word').value;
+  console.log(term);
+  fetchData(term);
+}
+
+//wordlist
+
+function appendWord() {
+  const wordListElement = document.getElementById('wordlist');
+  const inputWordValue = document.getElementById('input-word').value;
+  console.log(inputWordValue);
+
+  const liElement = document.createElement('li');
+
+  const word = document.createTextNode(inputWordValue);
+  liElement.appendChild(word);
+  console.log(word);
+
+  wordListElement.appendChild(liElement);
+  console.log(liElement);
+
+}

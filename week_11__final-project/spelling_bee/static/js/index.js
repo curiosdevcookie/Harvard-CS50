ebfe169c24b3564965868ebbf8dabd62f765191a
Ariadne Engelbrecht <ariadneengelbrecht@dionysos.local>
2023-06-12 12:02:53 +0200

    Add instructions-dialog.

--- week_11__final-project/spelling_bee/static/js/index.js
+++ week_11__final-project/spelling_bee/static/js/index.js
@@ -23,3 +23,15 @@ function deleteLetter() {
   const input = document.getElementById('input-word');
   input.value = input.value.slice(0, -1);
 }
+
+// Dialog #dialog-instructions handling
+// If user closes the dialog, the section #instructions will be added to the DOM:
+const dialog = document.getElementById('dialog-instructions');
+if (dialog) {
+  dialog.addEventListener('close', () => {
+    const instructions = document.getElementById('instructions');
+    instructions.style.display = 'block';
+    // Also an animation will be added to the section #instructions that takes 3 seconds:
+    instructions.style.animation = 'drop_into_view 3s';
+  });
+}
